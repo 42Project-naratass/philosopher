5308df36f32480e5c86bcc6fa60e938cf61c5a2e
fuku <killvader8550@gmail.com>
2026-06-09 21:24:16 +0700

    Update: philo.h

--- philo/philo.h
+++ philo/philo.h
@@ -4,12 +4,15 @@
 # include <stdlib.h>
 # include <pthread.h>
 # include <unistd.h>
+# include <stdbool.h>
+# include "err.h"
 
 // utils
-size_t	ft_strlen(const char *s);
+size_t	ft_strlen(char *s);
 void	putstr_fd(char *str, int fd);
 double	ft_atof(char *nbtr);
 double	ft_strtod(char *str, char **endptr);
-int	ft_atoi(const char *nptr);
+int		ft_atoi(char *nptr);
+bool	check_nums(char *str);
 
 #endif
