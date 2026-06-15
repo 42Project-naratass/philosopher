d66ba53a6cd684f639feb52a872efd6fedd9c240
fuku <killvader8550@gmail.com>
2026-06-15 15:31:20 +0700

    Add: ft_memcpy.c

---
+++ philo/utils/ft_memcpy.c
@@ -0,0 +1,15 @@
+#include "../philo.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*pdest;
+	unsigned char	*psrc;
+
+	if (!dst && !src)
+		return (NULL);
+	pdest = (unsigned char *)dst;
+	psrc = (unsigned char *)src;
+	while (n--)
+		*pdest++ = *psrc++;
+	return (dst);
+}
