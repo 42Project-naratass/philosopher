9be619d0a9ceb4cdd6cebfe78f1bd79156ffe2d0
fuku <killvader8550@gmail.com>
2026-07-14 08:45:22 +0700

    Update: log color

--- philo/message/print_status.c
+++ philo/message/print_status.c
@@ -2,7 +2,7 @@
 
 static void	print_log(size_t timestamp, size_t id, char *status)
 {
-	printf("%zu %zu %s\n", timestamp, id, status);
+	printf(Y"%zu\t" RST "%zu\t%s\n", timestamp, id, status);
 }
 
 void	print_status(t_philo *philo, t_status mode, size_t timestamp)
@@ -16,14 +16,14 @@ void	print_status(t_philo *philo, t_status mode, size_t timestamp)
 		return ;
 	}
 	if (mode == SLEEPING)
-		print_log(timestamp, philo->id, "is sleeping");
+		print_log(timestamp, philo->id, M "is sleeping" RST);
 	else if (mode == EATING)
-		print_log(timestamp, philo->id, "is eating");
+		print_log(timestamp, philo->id, C "is eating" RST);
 	else if (mode == THINKING)
-		print_log(timestamp, philo->id, "is thinking");
+		print_log(timestamp, philo->id, G "is thinking" RST);
 	else if (mode == GET_LEFT_FORK)
-		print_log(timestamp, philo->id, "has taken a fork");
+		print_log(timestamp, philo->id, BK "has taken a fork" RST);
 	else if (mode == GET_RIGHT_FORK)
-		print_log(timestamp, philo->id, "has taken a fork");
+		print_log(timestamp, philo->id, BK "has taken a fork" RST);
 	pthread_mutex_unlock(&philo->data->print);
 }
